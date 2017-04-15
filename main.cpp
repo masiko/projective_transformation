d20b04db7ff3a88560be2d11005286897f34c8dd
masiko <masiko.taiitirou@gmail.com>
2017-04-16 03:29:59 +0900

    add projective_tf class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
-#include<iostream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
-// tmp of image processing program
+#include <iostream>
+#include "projective_tf.hpp"
 
 int setOutput(int x, int y, int col, int row, char b, char g, char r, cv::Mat out) {
 	if (0<=x && x<col && 0<=y && y<row) {
@@ -14,20 +11,9 @@ int setOutput(int x, int y, int col, int row, char b, char g, char r, cv::Mat ou
 }
 
 int main() {
-	cv::Mat in;
-	cv::Mat out;
-//	cv::Mat rtm;
-//	rtm = (cv::Mat_<double>(3,3) << 1.0, .0, .0, .0, 1/cos(M_PI/3.0), .0, .0, .0, 1.0);
-	double rtm[9] = {1.0, .0, .0, .0,  1/cos(4*M_PI/10.0), .0, .0, .0, 1.0};
-	// rotation around x axis
-	double stm[9] = {1.0, .0, .0, .0, 1.0, .0, .0, .0, 1.0};
-	//
-
+	projective_tf pt;
 	int x, y, t;
 	int x0, y0, t0;
-	int row, col;
-	double r0, r1, rz0, rz1;
-	double a;
 
 	cv::VideoCapture cap(1);
 	cv::namedWindow("cap");
@@ -36,39 +22,18 @@ int main() {
 	if (!cap.isOpened())	return -1;
 	std::cout << "start cap channel 1\n";
 
-	row = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
-	col = cap.get(CV_CAP_PROP_FRAME_WIDTH);
-	out.create(row, col, CV_8UC3);
-	stm[5] = -row;		//the tekito
-
-	for (int i=0; i<3; i++) {
-		for (int j=0; j<3; j++) {
-			a = rtm[j + i*3];
-			std::cout << a << " ";
-		}
-		std::cout<<std::endl;
-	}
+	pt.setMatPra(cap.get(CV_CAP_PROP_FRAME_WIDTH), cap.get(CV_CAP_PROP_FRAME_HEIGHT));
+	pt.out.create((int)cap.get(CV_CAP_PROP_FRAME_HEIGHT), (int)cap.get(CV_CAP_PROP_FRAME_WIDTH), CV_8UC3);
+	
+	pt.setFOV(2.0);
 
 	while(1) {
-		cap >> in;
+		cap >> pt.in;
 		//write main loop
-		for (int i=row/4; i<3*row/4; i++) {			//y
-			stm[0] = -2.0/row*i + 2.5;
-			stm[2] = (double)(-col*stm[0]/2.0+col/2);
-			for (int j=col/4; j<3*col/4; j++) {		//x
-				x0 = (int)(rtm[0]*j + rtm[1]*i + rtm[2]);
-				y0 = (int)(rtm[3]*j + rtm[4]*i + rtm[5]);
-				t0 = (int)(rtm[6]*j + rtm[7]*i + rtm[8]);
-				x = (int)(stm[0]*x0 + stm[1]*y0 + stm[2]);
-				y = (int)(stm[3]*x0 + stm[4]*y0 + stm[5]);
-				t = (int)(stm[6]*x0 + stm[7]*y0 + stm[8]);
+		pt.culTf();
 
-				setOutput(x, y, col, row, in.data[3*(i*col+j)], in.data[3*(i*col+j)+1], in.data[3*(i*col+j)+2], out);
-			}
-		}
-
-		cv::imshow("cap",in);
-		cv::imshow("dst",out);
+		cv::imshow("cap",pt.in);
+		cv::imshow("dst",pt.out);
 		if (cv::waitKey(100)!=-1)	break;
 	}
 
---
+++ projective_tf.cpp
@@ -0,0 +1,72 @@
+#include "projective_tf.hpp"
+
+projective_tf::projective_tf() {
+	width =
+	height =
+	centerx = 
+	centery = -1;
+
+	tilt = 4*M_PI/10;	//0 < tilt < M_PI/2
+	FOV = M_PI/3;
+}
+
+int projective_tf::setMatPra(int w, int h) {
+	width = w;
+	height = h;
+	centerx = w/2;
+	centery = h/2;
+	startx = w/4;
+	starty = h/4;
+	endx = 3*w/4;
+	endy = 3*h/4;
+
+	a = 
+	b = 
+	c = 
+	d = .0;
+
+	rm[4] = 1/cos(tilt);
+	sm[5] = -rm[4]*centery + centery;
+	return 0;
+}
+
+int projective_tf::setFOV(double ang) {
+	a = (1.0 - ang) / (endy - starty);
+	b = a*starty + ang;
+}
+
+int projective_tf::culTf() { 
+	double v0[3] = {.0, .0, 1.0};
+	double v1[3] = {.0 ,.0, 1.0};
+	double v2[3] = {.0, .0, 1.0};
+	for (v0[1]=starty; v0[1]<endy; v0[1]++) {
+		culFOV(v0[1]);
+		for (v0[0]=startx; v0[0]<endx; v0[0]++) {
+			culMt(rm, v0, v1);
+			culMt(sm, v1, v2);
+			setOutput(v0, v2);
+		}
+	}
+	return 0;
+}
+
+int projective_tf::culMt(double* m, double* vi, double* vo) {
+	vo[0] = m[0]*vi[0] + m[1]*vi[1] + m[2]*vi[2];
+	vo[1] = m[3]*vi[0] + m[4]*vi[1] + m[5]*vi[2];
+	vo[2] = m[6]*vi[0] + m[7]*vi[1] + m[8]*vi[2];
+	return 0;
+}
+
+int projective_tf::culFOV(double h) {
+	sm[0] = a*h + b;
+	sm[2] = -sm[0]*centerx + centerx;
+	return 0;
+}
+
+int projective_tf::setOutput(double* vi, double* vo) {
+	if ( vo[0] < 0 || width <= vo[0] || vo[1] < 0 || height <= vo[1] )	return 1;
+	out.data[3*((int)vo[0] + width*(int)vo[1])] = in.data[3*((int)vi[0] + width*(int)vi[1])];
+	out.data[3*((int)vo[0] + width*(int)vo[1]) + 1] = in.data[3*((int)vi[0] + width*(int)vi[1]) + 1];
+	out.data[3*((int)vo[0] + width*(int)vo[1]) + 2] = in.data[3*((int)vi[0] + width*(int)vi[1]) + 2];
+	return 0;
+}
